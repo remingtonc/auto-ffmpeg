026f755805e2776fe1ca802b21e0139c77b594f3
Remington <code@remington.io>
2019-09-04 03:38:52 +0000

    Using CRF, no 2 pass

--- test.sh
+++ test.sh
@@ -4,8 +4,7 @@ TARGET_PATH="/datalake/staging/transcode/target"
 for VIDEO_FILE_PATH in $SOURCE_PATH/*.mkv; do
 	echo "SOURCE: $VIDEO_FILE_PATH"
 	VIDEO_FILENAME=${VIDEO_FILE_PATH##*/}
-	echo "Making first pass on $VIDEO_FILENAME"
-	docker run --rm -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target --entrypoint "/bin/bash" ffmpeg -c "ffmpeg -y -i \"/data/source/${VIDEO_FILENAME}\" -c:v libx265 -crf 20 -preset slow -x265-params pass=1 -an -f matroska /dev/null && ffmpeg -i \"/data/source/${VIDEO_FILENAME}\" -c:v libx265 -crf 20 -preset slow -x265-params pass=2 -c:a libfdk_aac -vbr 5 \"/data/target/${VIDEO_FILENAME}\""
+	docker run --rm -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target ffmpeg -i "/data/source/${VIDEO_FILENAME}" -c:v libx265 -crf 20 -preset slow -c:a libfdk_aac -vbr 5 "/data/target/${VIDEO_FILENAME}"
 	echo "TARGET: $TARGET_PATH/$VIDEO_FILENAME"
 done
 
