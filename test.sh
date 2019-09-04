51c4517cbecfc416e463f5b1872f1b520c01c006
Remington <code@remington.io>
2019-09-04 03:43:41 +0000

    Attempt to copy subtitles

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ TARGET_PATH="/datalake/staging/transcode/target"
 for VIDEO_FILE_PATH in $SOURCE_PATH/*.mkv; do
 	echo "SOURCE: $VIDEO_FILE_PATH"
 	VIDEO_FILENAME=${VIDEO_FILE_PATH##*/}
-	docker run --rm -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target ffmpeg -i "/data/source/${VIDEO_FILENAME}" -c:v libx265 -crf 20 -preset slow -c:a libfdk_aac -vbr 5 "/data/target/${VIDEO_FILENAME}"
+	docker run --rm -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target ffmpeg -i "/data/source/${VIDEO_FILENAME}" -c:v libx265 -crf 20 -preset slow -c:a libfdk_aac -vbr 5 -c:s copy "/data/target/${VIDEO_FILENAME}"
 	echo "TARGET: $TARGET_PATH/$VIDEO_FILENAME"
 done
 
