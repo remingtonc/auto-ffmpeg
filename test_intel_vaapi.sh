bc822185f0579bf5bc7d671d5e83839d4d0870fe
Remington Campbell <code@remington.io>
2023-01-11 16:53:02 -0800

    change format in vaapi and remove crf values

--- test_intel_vaapi.sh
+++ test_intel_vaapi.sh
@@ -4,7 +4,7 @@ TARGET_PATH="/datalake/staging/transcode/target"
 for VIDEO_FILE_PATH in $SOURCE_PATH/*.mkv; do
         echo "SOURCE: $VIDEO_FILE_PATH"
         VIDEO_FILENAME=${VIDEO_FILE_PATH##*/}
-        podman run -it --rm --group-add keep-groups --device=/dev/dri:/dev/dri -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target --entrypoint=ffmpeg localhost/ffmpeg-qs -vaapi_device /dev/dri/renderD128 -i "/data/source/${VIDEO_FILENAME}" -c:v hevc_vaapi -crf 20 -preset slow -c:a libfdk_aac -vbr 5 -c:s copy "/data/target/vaapi_${VIDEO_FILENAME}"
+        podman run -it --rm --group-add keep-groups --device=/dev/dri:/dev/dri -v $SOURCE_PATH:/data/source -v $TARGET_PATH:/data/target --entrypoint=ffmpeg localhost/ffmpeg-qs -vaapi_device /dev/dri/renderD128 -i "/data/source/${VIDEO_FILENAME}" -vf 'format=p010,hwupload' -c:v hevc_vaapi -preset slow -c:a libfdk_aac -vbr 5 -c:s copy "/data/target/vaapi_${VIDEO_FILENAME}"
         echo "TARGET: $TARGET_PATH/$VIDEO_FILENAME"
         sleep 10
 done
