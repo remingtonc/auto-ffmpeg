073a1e9399ff5c9035c56e9b6c3a454f41241891
Remington Campbell <code@remington.io>
2023-03-15 18:24:03 -0700

    it works

---
+++ makemkv/backup_disc.sh
@@ -0,0 +1,1 @@
+podman run -it --rm --group-add keep-groups --device=/dev/dri:/dev/dri --device=/dev/cdrom:/dev/cdrom --device=/dev/sg4:/dev/sg4 -v /datalake/staging/transcode/source:/data/source localhost/makemkv-linux --robot --decrypt --cache=1024 --minlength=600 mkv disc:0 all /data/source
