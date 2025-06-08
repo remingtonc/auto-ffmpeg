03477caff0b3fd0727118014730d16d592e31602
Remington Campbell <code@remington.io>
2025-06-08 12:32:59 -0700

    fix makemkv image built reference in backup script

--- makemkv/backup_disc.sh
+++ makemkv/backup_disc.sh
@@ -1,1 +1,2 @@
-podman run -it --rm --group-add keep-groups --device=/dev/dri:/dev/dri --device=/dev/cdrom:/dev/cdrom --device=/dev/sg4:/dev/sg4 -v /datalake/staging/transcode/source:/data/source localhost/makemkv-linux --robot --decrypt --cache=1024 --minlength=600 mkv disc:0 all /data/source
+#!/usr/bin/env bash
+podman run -it --rm --group-add keep-groups --device=/dev/dri:/dev/dri --device=/dev/cdrom:/dev/cdrom --device=/dev/sg4:/dev/sg4 -v /datalake/staging/transcode/source:/data/source localhost/auto-ffmpeg-makemkv:1.18.1 --robot --decrypt --cache=1024 --minlength=600 mkv disc:0 all /data/source
